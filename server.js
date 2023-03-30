4d6aabd44994b474917dfe04cbf9187fb46b6b00
sheelaadhikari <adhikarisheela1000@gmail.com>
2023-03-30 13:46:39 +0545

    done basic server setup

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import colors from 'colors';
+import dotenv from 'dotenv';
+//configure env
+dotenv.config();
 
 //rest object
 const app = express();
 //rest api
 app.get('/', (req, res) => {
     res.send("<h1>welcome to this project</h1>")
-})
+});
 //port
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 //run listen
 app.listen(PORT, () => {
-    console.log(`server is running on ${PORT}`.bgCyan.white);
+    console.log(`server is running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
 })
