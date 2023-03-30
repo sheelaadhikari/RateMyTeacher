8e1299a7d84f4661a348b4c00bc70384b46f02c9
sheelaadhikari <adhikarisheela1000@gmail.com>
2023-03-30 17:42:21 +0545

    done mongodb database setup

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
 //configure env
 dotenv.config();
 
+//database config
+connectDB();
+
 //rest object
 const app = express();
+
+//middleware
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+
+
 //rest api
 app.get('/', (req, res) => {
     res.send("<h1>welcome to this project</h1>")
