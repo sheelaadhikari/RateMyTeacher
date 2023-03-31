6b1f0d1f3a55a3621aa182a8d436f85c1b84c102
sheelaadhikari <adhikarisheela1000@gmail.com>
2023-03-31 14:09:48 +0545

    models and hashing

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URL);
 
-        console.log(`Connected to Mongodb Database ${conn.connection.host}`);
+        console.log(`Connected to Mongodb Database ${conn.connection.host}`.bgMagenta.white);
 
     }
     catch (error) {
