81456f93c0c4a24ec78bebec5eb176d469f6942e
sheelaadhikari <adhikarisheela1000@gmail.com>
2023-04-08 15:24:46 +0545

    creating different react pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 
 import './App.css';
+import Footer from './components/Layout/Footer';
+import Header from './components/Layout/Header';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
-    <>
+    <Layout>
+
       <h1>teacher rating app</h1>
-    </>
+
+    </Layout>
+
   );
 }
 
---
+++ client/src/components/Layout/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div><h1>Header Comp</h1></div>
+    )
+}
+
+export default Header
