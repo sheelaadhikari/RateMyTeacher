f530e093532dbf1874336d10a85f07579b7ac661
sheelaadhikari <adhikarisheela1000@gmail.com>
2023-04-10 21:02:08 +0545

    added nav menu

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import About from './../../pages/About';
+import Teachers from './../../pages/Teachers';
 
 const Footer = () => {
     return (
-        <div className='bg-dark text-light p-3' >
+        <div className='footer' >
             <h4 className='text-center'> All Right Reserved &copy; RateMyTeacher</h4>
+            <p className="text-center mt-3" >
+                <Link to='/about'>About</Link>|
+                <Link to='/policy'>PrivacyPolicy</Link>|
+                <Link to='/contact'>Contact</Link>|
+
+            </p>
+
         </div>
     );
 };
--- client/src/pages/Pagenotfound.js
+++ client/src/pages/Pagenotfound.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Layout from './../components/Layout/Layout';
+import { Link } from 'react-router-dom';
 
 const Pagenotfound = () => {
     return (
-        <Layout><h1>Pagenotfound</h1></Layout>
-    )
-}
+        <Layout>
+            <div className='pnf'>
+                <h1 className='pnf-title'>404</h1>
+                <h2 className='pnf-heading'>Oops! page not found</h2>
+                <Link t0='/' className='pnf-btn'>Go Back</Link>
+            </div>
+        </Layout>
+    );
+};
 
 export default Pagenotfound;
