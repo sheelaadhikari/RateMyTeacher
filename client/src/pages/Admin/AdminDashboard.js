a933a28a8d1f401943f0f3d071a949062469111b
sheelaadhikari <adhikarisheela1000@gmail.com>
2023-05-11 18:05:02 +0545

    admin dashboard

--- client/src/pages/Admin/AdminDashboard.js
+++ client/src/pages/Admin/AdminDashboard.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
+import AdminMenu from '../../components/Layout/AdminMenu'
 
 const AdminDashboard = () => {
     return (
-        <Layout><h1>AdminDashboard</h1></Layout>
+        <Layout>
+
+            <div className='container-fluid m-3 p-3'>
+                <div className='row'>
+                    <div className='col-md-3'>
+                        <AdminMenu />
+                    </div>
+                    <div className='col-md-9'> Content</div>
+
+
+                </div>
+
+            </div>
+
+
+        </Layout>
     )
 }
 
