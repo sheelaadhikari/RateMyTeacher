41564d2ed4d6bd176927d07337af301fc4666fb0
sheelaadhikari <adhikarisheela1000@gmail.com>
2023-12-03 14:54:55 +0545

    highest rated teachers

--- client/src/pages/Ratings.js
+++ client/src/pages/Ratings.js
@@ -3,6 +3,7 @@ import { useAuth } from "../context/auth";
 import "../../src/index.css";
 import React, { useState, useEffect } from "react";
 import Layout from "./../components/Layout/Layout";
+import RatingDisplayBox from "./user/components/RatingDisplayBox";
 
 const Ratings = () => {
   const [auth, setAuth] = useAuth();
@@ -34,18 +35,25 @@ const Ratings = () => {
     <div>
       Ratings
       <Layout>
-        <div className="list-of-AllTeachersRatings">
+        <div className="list-of-teachers">
           {loading ? "loading" : "List Of All TeachersRatings"}
         </div>
         <div className="container-box">
-          {ratings?.map((r) => (
-            <div className="h-ratings">
-              <div>
-                <p className="card-title">{r.overallRating}</p>
-                <p className="card-text"> {r.teacher[0].name}</p>
+          {ratings
+            ?.sort((a, b) => b.overallRating - a.overallRating) // Sort ratings in descending order
+            .map((r) => (
+              <div className="h-ratings" key={r}>
+                <div style={{ fontWeight: "bold", color: "black" }}>
+                  <img src={`/api/v1/teacher/teacher-photo/${r._id}`}></img>
+                  <div className="card-text">
+                    <RatingDisplayBox rating={r.overallRating} />
+                  </div>
+                  <p className="card-text">Name : {r.teacher[0].name}</p>
+                  <p className="card-text">Subject : {r.teacher[0].subject}</p>
+                  <p className="card-text">Bio : {r.teacher[0].bio}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       </Layout>
     </div>
